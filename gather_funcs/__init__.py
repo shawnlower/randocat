c93a078f9c2361d247de99af2be9cc08e953724a
Shawn Lower <shawn@shawnlower.com>
2016-11-02 23:52:54 -0400

    Update with new modules

--- gather_funcs/__init__.py
+++ gather_funcs/__init__.py
@@ -1,5 +1,8 @@
 #from gather_fake import FakeEntropyGatherer
 #from gather_fake import FastFakeEntropyGatherer
 
-from gather_linux import RandomEntropyGatherer
-from gather_linux import UrandomEntropyGatherer
+#from gather_linux import RandomEntropyGatherer
+#from gather_linux import UrandomEntropyGatherer
+
+from gather_utils import ProcEntropyGatherer
+from gather_utils import SockstatEntropyGatherer
